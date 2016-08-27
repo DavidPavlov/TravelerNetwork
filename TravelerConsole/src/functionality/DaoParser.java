1f05a22ce17d81e65df538662ad23bee9f75b612
DavidPavlov <ahil434@gmail.com>
2016-08-27 15:43:08 +0300

    Corrected some small mistakes :)

--- TravelerConsole/src/functionality/DaoParser.java
+++ TravelerConsole/src/functionality/DaoParser.java
@@ -1,19 +1,45 @@
 package functionality;
 
-import java.util.HashMap;
-
-import dao.UserDao;
-import exceptions.InvalidAuthorException;
-import exceptions.InvalidCoordinatesException;
-import exceptions.InvalidDataException;
-import models.Comment;
-import models.Destination;
-import models.Location;
-import models.PlaceToEat;
-import models.PlaceToSleep;
 import models.User;
 
 public class DaoParser {
 
-	
+	public static boolean addComment(User user, String comment) {
+		return true;
+		// TODO
+	}
+
+	public static boolean addDestination(User user, String name, String description, double longtitude,
+			double lattitude) {
+		return true;
+		// TODO
+	}
+
+	public static boolean addHotel(User user, String name, double longtitude, double lattitude, String contact) {
+		return true;
+		// TODO
+	}
+
+	public static boolean addResturant(User user, String name, double longtitude, double lattitude,
+			String workingHours) {
+		return true;
+		// TODO
+	}
+
+	public static User logIn(String email, String password) {
+		return null;
+		// TODO
+	}
+
+	public static boolean logOut() {
+		return true;
+		// TODO
+	}
+
+	public static boolean registerUser(String firstName, String lastName, String email, String password,
+			String description) {
+		return true;
+		// TODO
+	}
+
 }
